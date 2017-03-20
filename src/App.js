02ba381f2dc8d4cf403e6d14c80ab862d67c4ae3
Karl <karl.l1@hotmail.com>
2017-03-20 16:24:59 +1100

    Initial

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Near, the local thing finder</h2>
+            <h2>Welcome to Near, the local thing finder</h2>
+            <form>
+            
+              <input id="mainInput" type="text" name="name" placeholder="Where U at?" />
+            
+            <button id="YOLO" type="submit"> SUBMIT </button>
+          </form>
+          
+          
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
