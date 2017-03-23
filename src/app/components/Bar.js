30807804a8d9404ee1176c4607f5dda0e15492c2
Karl <karl.l1@hotmail.com>
2017-03-24 01:48:26 +1100

    Search bar

--- src/app/components/Bar.js
+++ src/app/components/Bar.js
@@ -4,7 +4,7 @@ class Bar extends React.Component {
     render() {
         return (
             <div className="bar overlay">
-
+                <h1>Find restaurants nearby</h1>
             </div>
         );
     }
